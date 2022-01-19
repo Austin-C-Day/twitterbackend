7a0252c449f2a1f2c4cb5e4323e4460098787766
Austin <trustcompany1964@gmail.com>
2022-01-18 20:53:12 -0500

    Created User class, created get endpoint to get test database

---
+++ classes/user.js
@@ -0,0 +1,9 @@
+class User{
+    constructor(userId, username, email){
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+}
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const User = require("./classes/user")
+
+
+var userDatabase = [
+  new User("1", "DonkeyMan127", "Donkey@gmail.com"),
+  new User("2", "LouieBOI73", "DadsMustache@gmail.com"),
+  new User("3", "JakeyPoo", "feetguy@feetlover.com")
+];
+
+
+
+app.get('/users', (req, res) => {
+  res.send(userDatabase)
+})
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello')
 })
 
-app.post('/post', function (req, res) {
-    res.send('Got a POST request')
-  })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
